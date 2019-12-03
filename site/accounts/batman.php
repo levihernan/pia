7f6cb38ddaf52dc265c9a749f9303e7eafa00e31
Hernan Levi <levi.hernan@gmail.com>
2019-12-03 17:43:45 +0100

    accounts

---
+++ site/accounts/batman.php
@@ -0,0 +1,12 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: batman
+password: >
+  $2y$10$rX831I6PbNaR1KQveCiEY.bqksesrQtnF9.mXblpaFcamPtihwSpe
+email: nan@maran.co
+language: es_ES
+role: admin
+history:
+  - home
+  - projects/barca
+  - projects
---
+++ site/accounts/pia.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: pia
+firstname: PIA
+lastname: Advisors
+email: pia@pia.com
+password: >
+  $2y$10$CWSi8IpzxC7B2ErDwaV8b./p2/gtImFGKB8pFc4L0OadrDwG4kq0G
+language: es_ES
+role: editor
